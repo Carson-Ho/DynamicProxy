e9d2ad2cdfad286afb2ae60231670383aa3a31e1
Carson.Ho <carson.ho@foxmail.com>
2017-07-25 15:01:59 +0800

    modify bug

--- app/src/main/java/scut/carson_ho/dynamicproxy/MainActivity.java
+++ app/src/main/java/scut/carson_ho/dynamicproxy/MainActivity.java
@@ -10,16 +10,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // 1. 创建动态代理类对象
+        // 1. 创建调用处理器类对象
         DynamicProxy DynamicProxy = new DynamicProxy();
-        // 2. 根据传入的目标对象实例，为其创建动态代理对象
-        // 2.1 代购为小成代购Mac
-        Subject Buyer1_DynamicProxy = (Subject) DynamicProxy.newProxyInstance(new Buyer1());
-        Buyer1_DynamicProxy.buybuybuy();
 
-        // 2.2 代购为小何代购iPhone
-        Subject Buyer2_DynamicProxy = (Subject) DynamicProxy.newProxyInstance(new Buyer2());
+        // 2. 创建目标对象实例
+        Buyer1 mBuyer1 = new Buyer1();
+
+        // 3. 创建动态代理类 & 对象：通过调用处理器类对象newProxyInstance（）
+        // 传入上述目标对象实例
+        Subject Buyer1_DynamicProxy = (Subject) DynamicProxy.newProxyInstance(mBuyer1);
+
+        // 4. 通过调用动态代理对象方法从而调用目标对象方法
+        // 实际上是调用了invoke（），再通过invoke（）里的反射机制调用目标对象的方法
+        Buyer1_DynamicProxy.buybuybuy();
+        // 以上代购为小成代购Mac
+
+        // 以下是代购为小何代购iPhone
+        Buyer2 mBuyer2 = new Buyer2();
+        Subject Buyer2_DynamicProxy = (Subject) DynamicProxy.newProxyInstance(mBuyer2);
         Buyer2_DynamicProxy.buybuybuy();
     }
 }
